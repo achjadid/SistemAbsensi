50ccd5d4617cc407bf93222261e5d17a87f8e3af
jadidampme <32932076+jadidampme@users.noreply.github.com>
2023-01-02 15:20:38 +0700

    API Insert & Update Department

--- API/Controllers/DepartmentsController.cs
+++ API/Controllers/DepartmentsController.cs
@@ -1,8 +1,10 @@
-﻿using API.Repositories;
+﻿using API.Models;
+using API.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Net;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace API.Controllers
 {
@@ -31,17 +33,45 @@ namespace API.Controllers
             }
         }
 
-        [HttpGet("{id}")]
-        public virtual ActionResult Get(int id)
+        //[HttpGet("{id}")]
+        //public virtual ActionResult Get(int id)
+        //{
+        //    var get = repository.GetByRole(id);
+        //    if (get.Count() != 0)
+        //    {
+        //        return StatusCode(200, new { status = HttpStatusCode.OK, message = get.Count() + " Data Ditemukan", Data = get });
+        //    }
+        //    else
+        //    {
+        //        return StatusCode(404, new { status = HttpStatusCode.NotFound, message = get.Count() + " Data Ditemukan", Data = get });
+        //    }
+        //}
+
+        [HttpPost]
+        public virtual ActionResult Insert(Department department)
         {
-            var get = repository.GetByRole(id);
-            if (get.Count() != 0)
+            var insert = repository.Insert(department);
+            if (insert >= 1)
             {
-                return StatusCode(200, new { status = HttpStatusCode.OK, message = get.Count() + " Data Ditemukan", Data = get });
+                return StatusCode(200, new { status = HttpStatusCode.OK, message = "Data Berhasil Dimasukkan", Data = insert });
             }
             else
             {
-                return StatusCode(404, new { status = HttpStatusCode.NotFound, message = get.Count() + " Data Ditemukan", Data = get });
+                return StatusCode(500, new { status = HttpStatusCode.InternalServerError, message = "Gagal Memasukkan Data", Data = insert });
+            }
+        }
+
+        [HttpPut]
+        public virtual ActionResult Update(Department department)
+        {
+            var update = repository.Update(department);
+            if (update >= 1)
+            {
+                return StatusCode(200, new { status = HttpStatusCode.OK, message = "Data Berhasil Diperbaharui", Data = update });
+            }
+            else
+            {
+                return StatusCode(500, new { status = HttpStatusCode.InternalServerError, message = "Gagal Memperbaharui Data", Data = update });
             }
         }
     }
--- API/Repositories/DepartmentRepository.cs
+++ API/Repositories/DepartmentRepository.cs
@@ -5,8 +5,10 @@ using API.ViewModels;
 using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Xml.Linq;
 
 namespace API.Repositories
 {
@@ -26,37 +28,52 @@ namespace API.Repositories
         {
             using (SqlConnection connection = new SqlConnection(_configuration["ConnectionStrings:APISistemAbsensi"]))
             {
-                //var spName = "SP_DepartmentsGetAll";
-                //var res = connection.Query<Department>(spName, commandType: CommandType.StoredProcedure);
-                //return res;
-                throw new System.NotImplementedException();
+                var spName = "SP_DepartmentsGetAll";
+                var res = connection.Query<Department>(spName, commandType: CommandType.StoredProcedure);
+                return res;
+                //throw new System.NotImplementedException();
             }
         }
 
-        public virtual IEnumerable<DepartmentVM> GetByRole(int Id)
-        {
-            using (SqlConnection connection = new SqlConnection(_configuration["ConnectionStrings:APISistemAbsensi"]))
-            {
-                var spName = "SP_DepartmentsGetAll";
-                parameters.Add("@RoleId", Id);
-                var res = connection.Query<DepartmentVM>(spName, parameters, commandType: CommandType.StoredProcedure);
-                return res;
-            }
-        }
+        //public virtual IEnumerable<DepartmentVM> GetByRole(int Id)
+        //{
+        //    using (SqlConnection connection = new SqlConnection(_configuration["ConnectionStrings:APISistemAbsensi"]))
+        //    {
+        //        var spName = "SP_DepartmentsGetAll";
+        //        parameters.Add("@RoleId", Id);
+        //        var res = connection.Query<DepartmentVM>(spName, parameters, commandType: CommandType.StoredProcedure);
+        //        return res;
+        //    }
+        //}
 
         public Department Get(int key)
         {
             throw new System.NotImplementedException();
         }
 
-        public int Insert(Department entity)
+        public int Insert(Department department)
         {
-            throw new System.NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_configuration["ConnectionStrings:APISistemAbsensi"]))
+            {
+                var spName = "SP_DepartmentsInsert";
+                parameters.Add("@DepartmentName", department.DepartmentName);
+                parameters.Add("@SupervisorNIK", department.SupervisorNIK);
+                var insert = connection.Execute(spName, parameters, commandType: CommandType.StoredProcedure);
+                return insert;
+            }
         }
 
-        public int Update(Department entity)
+        public int Update(Department department)
         {
-            throw new System.NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_configuration["ConnectionStrings:APISistemAbsensi"]))
+            {
+                var spName = "SP_DepartmentsUpdate";
+                parameters.Add("@Id", department.Id);
+                parameters.Add("@DepartmentName", department.DepartmentName);
+                parameters.Add("@SupervisorNIK", department.SupervisorNIK);
+                var update = connection.Execute(spName, parameters, commandType: CommandType.StoredProcedure);
+                return update;
+            }
         }
 
         public int Delete(int key)
